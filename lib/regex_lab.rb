5f9b656e9c0ab0de7eb18369f6acc5cf424d26c6
Adam Walker <adamw207@yahoo.com>
2020-02-19 17:21:25 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,18 @@
 def starts_with_a_vowel?(word)
-
+puts (word).scan(/\w+[aeiou])
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+puts (text).scan(/\un+ing/\)
 end
 
 def words_five_letters_long(text)
+puts (text).grep(/\w{5}\s/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+puts (text).match(/\W)
 end
 
 def valid_phone_number?(phone)
