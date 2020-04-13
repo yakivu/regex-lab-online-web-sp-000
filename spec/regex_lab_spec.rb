f5c508aabece521e17e02e7ae7730162ebf051eb
Adam Walker <adamw207@yahoo.com>
2020-04-13 17:54:41 +0000

    Automatically backed up by Learn

--- spec/regex_lab_spec.rb
+++ spec/regex_lab_spec.rb
@@ -64,7 +64,7 @@ describe "Working with Regular expressions" do
   describe "#valid_phone_number?" do
     it "returns true for valid phone numbers, regardless of formatting" do
       valid_numbers = ["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"]
-      expect(valid_numbers.all? { |number| valid_phone_number?(number) }).to be(true)
+      expect'(valid_numbers.all? { |number| valid_phone_number?(number) }).to be(true)'
     end
 
     it "returns false for invalid phone numbers, regardless of formatting" do
